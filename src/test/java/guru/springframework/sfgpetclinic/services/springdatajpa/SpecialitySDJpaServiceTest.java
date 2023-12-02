a38f221da7fb8a47b386cc2b3757c59ac4f35452
sahraoui-rami <149308696+sahraoui-rami@users.noreply.github.com>
2023-12-02 18:13:32 +0100

    Use the "given" method instead of the "when" method in the BDD.

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -61,34 +61,34 @@ class SpecialitySDJpaServiceTest {
 
     @Test
     void deleteById() {
-        service.deleteById(1l);
-        service.deleteById(1l);
+        service.deleteById(1L);
+        service.deleteById(1L);
 
-        verify(specialtyRepository, times(2)).deleteById(1l);
+        verify(specialtyRepository, times(2)).deleteById(1L);
     }
 
     @Test
     void deleteByIdAtLeast() {
-        service.deleteById(1l);
-        service.deleteById(1l);
+        service.deleteById(1L);
+        service.deleteById(1L);
 
-        verify(specialtyRepository, atLeastOnce()).deleteById(1l);
+        verify(specialtyRepository, atLeastOnce()).deleteById(1L);
     }
 
     @Test
     void deleteByIdAtMost() {
-        service.deleteById(1l);
-        service.deleteById(1l);
+        service.deleteById(1L);
+        service.deleteById(1L);
 
-        verify(specialtyRepository, atMost(5)).deleteById(1l);
+        verify(specialtyRepository, atMost(5)).deleteById(1L);
     }
 
     @Test
     void deleteByIdNever() {
-        service.deleteById(1l);
-        service.deleteById(1l);
+        service.deleteById(1L);
+        service.deleteById(1L);
 
-        verify(specialtyRepository, atLeastOnce()).deleteById(1l);
+        verify(specialtyRepository, atLeastOnce()).deleteById(1L);
 
         verify(specialtyRepository, never()).deleteById(5L);
     }
