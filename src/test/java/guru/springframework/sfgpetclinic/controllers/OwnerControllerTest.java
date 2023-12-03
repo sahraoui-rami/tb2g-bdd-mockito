edfe69c961184a27c8649c5c3d7b8e6d005097f3
sahraoui-rami <149308696+sahraoui-rami@users.noreply.github.com>
2023-12-03 17:01:19 +0100

    Capturing Mockito Argument.

--- src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -5,9 +5,14 @@ import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -29,6 +34,9 @@ class OwnerControllerTest {
 
     @Mock
     BindingResult result;
+
+    @Captor
+    ArgumentCaptor<String> stringArgumentCaptor;
     @Test
     void processCreationFormHasErrors() {
         // given
@@ -54,5 +62,35 @@ class OwnerControllerTest {
         // then
         assertThat(returnedView).isEqualTo(REDIRECT_OWNERS_5);
         then(service).should().save(any(Owner.class));
+    }
+
+    @Test
+    void processFindFormWildcardString() {
+        // given
+        Owner owner = new Owner(1L, "Joe", "Buck");
+        List<Owner> ownerList = new ArrayList<>();
+        ownerList.add(owner);
+        final ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        given(service.findAllByLastNameLike(captor.capture())).willReturn(ownerList);
+
+        // when
+        controller.processFindForm(owner, result, null);
+
+        // then
+        assertThat(captor.getValue()).isEqualTo("%Buck%");
+    }
+    @Test
+    void processFindFormWildcardStringAnnotation() {
+        // given
+        Owner owner = new Owner(1L, "Joe", "Buck");
+        List<Owner> ownerList = new ArrayList<>();
+        ownerList.add(owner);
+        given(service.findAllByLastNameLike(stringArgumentCaptor.capture())).willReturn(ownerList);
+
+        // when
+        controller.processFindForm(owner, result, null);
+
+        // then
+        assertThat(stringArgumentCaptor.getValue()).isEqualTo("%Buck%");
     }
 }
